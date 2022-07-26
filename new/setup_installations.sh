1bcb2a455cc0428f4e078bbbfb92e2392ddb8ee0
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-07-26 00:02:30 +0000

    chore: update installer file
    enable system configuration for the project
    
    step 7

--- new/setup_installations.sh
+++ new/setup_installations.sh
@@ -1,12 +1,35 @@
 # install python 3.7
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update -y
+sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev wget libbz2-dev
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz
+tar xvf Python-3.7.2.tgz
+cd Python-3.7.2
+./configure --enable-optimizations --enable-shared
+make
+sudo make install
+sudo apt install -y python3.7-venv libpython3.7 python3-virtualenv
+sudo apt auto-remove -y
+sudo apt update -y
+sudo apt upgrade -y
+cd ..
+python3 -m venv venv
+source venv/bin/activate
+virtualenv --clear venv
+sudo rm -rf Python-3.7.2*
+
+# install make
+sudo apt install make
 
 # install docker
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo usermod -aG docker $USER && newgrp docker
 
 # install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -16,12 +39,14 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 chmod +x kubectl
 mkdir -p ~/.local/bin
 mv ./kubectl ~/.local/bin/kubectl
+sudo rm -rf kubectl.sha256
 
 # install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-minikube start
+minikube start --driver=docker
+sudo rm -rf minikube-linux-amd64
 
 # install hadolint
-wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64
-chmod +x /bin/hadolint
+sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64
+sudo chmod +x /bin/hadolint
