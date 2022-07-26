3272417c44a18417dbb5355319df528972c6fbb9
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-07-26 13:37:31 +0000

    feat: dockerize flask application
    enable lightweight web applications and APIs in a fast and easy way
    
    step 9

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag udacity-devops-app:1.1 .
 
-# Step 2: 
+# Step 2:
 # List docker images
+docker images
 
-# Step 3: 
+# Step 3:
 # Run flask app
+docker run --name project-fourx -dp 8000:80 udacity-devops-app:1.1
