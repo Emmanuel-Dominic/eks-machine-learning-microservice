51ce1ba10026074af2e825c08b72aa8f10e4551d
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-07-26 14:18:50 +0000

    feat: deploy with kubernetes
    To automate deployment, scale, and manage containerized application
    
    step 13

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=manueldominic/udacity-devops-app:1.1
+echo "Docker ID and Image: $dockerpath"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-devops-minikube --image=manueldominic/udacity-devops-app:1.1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment udacity-devops-minikube --type=NodePort --port=80
