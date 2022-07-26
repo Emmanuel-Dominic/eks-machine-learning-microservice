2d8a9506631e87cf88a65249ba2239b62cc1dfd5
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-07-26 14:08:44 +0000

    feat: upload docker image
    This involves uploading a docker image on to dockerhub
    
    step 12

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=manueldominic/udacity-devops-app:1.1
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tag udacity-devops-app:1.1 manueldominic/udacity-devops-app:1.1
 
 # Step 3:
 # Push image to a docker repository
+docker image push manueldominic/udacity-devops-app:1.1
